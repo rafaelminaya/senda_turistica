100cb97c897d583dc2a4f4ae2fd3e922575a959b
Rafael Minaya <rafael_mr20@outlook.com>
2024-07-31 21:19:57 -0500

    Crear proyecto senda turistica

---
+++ src/main/java/com/rminaya/sendaturistica/SendaturisticaApplication.java
@@ -0,0 +1,13 @@
+package com.rminaya.sendaturistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendaturisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendaturisticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rminaya/sendaturistica/SendaturisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rminaya.sendaturistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendaturisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
